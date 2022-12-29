9a6db5521bb24394945d353c6218bd9fa8142feb
vivekChenna <chennavivek96@gmail.com>
2022-12-28 22:59:16 -0700

    added ErrorCodes file in the setup

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,13 @@
+const { StatusCodes } = require('http-status-codes')
+
+class AppError extends Error {
+  constructor(name, message, explanation, statusCode) {
+    super()
+    this.name = name
+    this.message = message
+    this.explanation = explanation
+    this.statusCode = statusCode
+  }
+}
+
+module.exports = AppError;
